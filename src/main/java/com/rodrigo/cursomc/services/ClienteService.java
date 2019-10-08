dc126ab78feb8ef2c8a78d1ecaeb58b51b1f8fa3
Rodrigo Lopes <rodrigovlskz@gmail.com>
2019-10-08 08:59:36 -0300

    ajustando e testando delete do cliente

--- src/main/java/com/rodrigo/cursomc/services/ClienteService.java
+++ src/main/java/com/rodrigo/cursomc/services/ClienteService.java
@@ -58,7 +58,7 @@ public class ClienteService {
 		repo.deleteById(id);
 		} 
 		catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivel excluir uma Cliente que possui entidades relacionadas");
+			throw new DataIntegrityException("Não é possivel excluir uma Cliente que possui pedidos relacionadas");
 		}
 	}
 	
